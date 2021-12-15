93e8f75cbf19ca13787d568dbcb91123be3a709f
Wahyu Maulana <wahyaumau@gmail.com>
2021-12-15 19:03:46 +0700

    prettier

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,35 +1,35 @@
 function navToggle() {
-  let listNav = document.querySelectorAll('nav ul')
+  let listNav = document.querySelectorAll("nav ul");
   for (let nav of listNav) {
-    nav.classList.toggle('show');      
+    nav.classList.toggle("show");
   }
 }
 
-function navBackground(){
+function navBackground() {
   let nav = document.querySelector("nav");
   const navHeight = nav.offsetHeight;
   let banner = document.querySelector(".banner");
   const bannerHeight = banner.offsetHeight;
-  const top =	 window.pageYOffset || document.documentElement.scrollTop;
-  if(window.innerWidth <= 786){
+  const top = window.pageYOffset || document.documentElement.scrollTop;
+  if (window.innerWidth <= 786) {
     nav.style.background = "black";
-  }else{
-    if (top > bannerHeight-navHeight) {
+  } else {
+    if (top > bannerHeight - navHeight) {
       nav.style.background = "black";
     } else {
       nav.style.background = "";
     }
-  }  
-}
-
-function fillSkillBar(){
-  let listSkillPercentage = document.querySelectorAll('.skill-percentage')
-  for (let skillPercentage of listSkillPercentage) {
-      skillPercentage.style.width = skillPercentage.innerHTML;
   }
 }
 
-document.addEventListener('DOMContentLoaded', fillSkillBar, false);
-document.querySelector('.hamburger-icon').addEventListener('click', navToggle);
-window.addEventListener('scroll', navBackground);
-window.addEventListener('resize', navBackground);
+function fillSkillBar() {
+  let listSkillPercentage = document.querySelectorAll(".skill-percentage");
+  for (let skillPercentage of listSkillPercentage) {
+    skillPercentage.style.width = skillPercentage.innerHTML;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", fillSkillBar, false);
+document.querySelector(".hamburger-icon").addEventListener("click", navToggle);
+window.addEventListener("scroll", navBackground);
+window.addEventListener("resize", navBackground);
